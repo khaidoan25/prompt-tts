dfec446164cbc79ad58ceab08024f59a200ed9b1
Khai Doan <khaidoan25@gmail.com>
2023-04-24 08:58:24 -0400

    add parameter to control the data generation step

--- data_preparation/generate_code.py
+++ data_preparation/generate_code.py
@@ -15,7 +15,7 @@ model.set_target_bandwidth(6.0)
 model.to(device)
 
 
-def create_batch(members, tf, batch_size):
+def create_batch(members, tf, batch_size, max_duration):
     index = 1
     batch = [[], [], []]    # wav, name, codec_length
     for member in members:
@@ -28,7 +28,10 @@ def create_batch(members, tf, batch_size):
         wav = convert_audio(wav, sr, model.sample_rate, model.channels)
         # Pad the wavfile to 20s duration
         batch[2].append(np.ceil(wav.shape[1] / 320))
-        wav = torch.cat([wav, torch.zeros((1, model.sample_rate*20-wav.shape[1]))], dim=-1)
+        wav = torch.cat(
+            [wav, torch.zeros((1, model.sample_rate*max_duration-wav.shape[1]))],
+            dim=-1
+        )
         wav = wav.unsqueeze(0)
         batch[0].append(wav)
         if index % batch_size == 0:
@@ -48,14 +51,14 @@ def generate(batch):
     return codes
 
 
-def main(input_file, batch_size):
+def main(input_file, batch_size, max_duration):
     output_file = input_file.replace(".tar", "_processed.tar")
     tf = tarfile.open(input_file, "r")
     members = tf.getmembers()
     print("Reading tarfile ...")
     
     output_tf = tarfile.open(output_file, "w")
-    for batch in tqdm(create_batch(members, tf, batch_size)):
+    for batch in tqdm(create_batch(members, tf, batch_size, max_duration)):
         codes = generate(batch[0])
         
         with TemporaryDirectory() as dirname:
@@ -87,9 +90,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Generate codec codes of waveform."
                                      "The data is stored in WebDataset format.")
     parser.add_argument('--input_file', type=str, default=None,
-                        help="Path to the input file", required=True)
+                        help="Path to the input file.", required=True)
     parser.add_argument('--batch_size', type=int, default=32,
-                        help="Batch size of Encodec encode")
+                        help="Batch size of Encodec encode.")
+    parser.add_argument('--max_duration', type=int, default=12,
+                        help="Maximum duration of an audio.")
 
 
     return parser.parse_args()
@@ -98,4 +103,4 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    main(args.input_file, args.batch_size)
+    main(args.input_file, args.batch_size, args.max_duration)
