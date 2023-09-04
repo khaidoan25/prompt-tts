7d1de273d9a1cc34c345e48f11e745bb167d1b76
Khai Doan <khaidoan25@gmail.com>
2023-09-04 07:47:25 -0400

    train with unet2dcondition

---
+++ train_multi_2d.py
@@ -0,0 +1,261 @@
+import logging
+import json
+import argparse
+import math
+from tqdm import tqdm
+from einops import rearrange
+
+from accelerate import Accelerator, DistributedDataParallelKwargs
+from diffusers import DDPMScheduler
+from diffusers.optimization import get_scheduler
+
+import torch
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.tensorboard import SummaryWriter
+
+from tts.model.models import TTSMultiSpeaker2D
+from tts.dataloader.dataloader import create_dataloader
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+
+
+def test_main(args):
+    config = json.load(open(args.config_file, "r"))
+    
+    model = TTSMultiSpeaker2D(config)
+    
+    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    accelerator = Accelerator(
+        gradient_accumulation_steps=config["gradient_accumulation_steps"],
+        kwargs_handlers=[ddp_kwargs]
+    )
+    
+    # data
+    dataloader = create_dataloader(
+        args.data_files,
+        args.batch_size,
+        args.max_seq_length,
+        data_type="multi_speaker",
+        shuffle=True
+    )
+    
+    num_update_steps_per_epoch = math.ceil(len(dataloader) / config["gradient_accumulation_steps"])
+    max_train_steps = config["num_train_epochs"] * num_update_steps_per_epoch
+    
+    # optimizer
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=1e-5,
+        betas=(0.95, 0.999),
+        weight_decay=1e-6,
+        eps=1e-8
+    )
+        
+    lr_scheduler = get_scheduler(
+        config["lr_scheduler"],
+        optimizer=optimizer,
+        num_warmup_steps=config["lr_warmup_steps"] * config["gradient_accumulation_steps"],
+        num_training_steps=max_train_steps * config["gradient_accumulation_steps"],
+    )
+    
+    dataloader, model, optimizer, lr_scheduler = accelerator.prepare(
+        dataloader, model, optimizer, lr_scheduler
+    )
+    
+    for epoch in range(config["num_train_epochs"]):
+        epoch += args.prev_epoch
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader, disable=not accelerator.is_local_main_process)
+        
+        for batch in pbar:
+            with accelerator.accumulate(model):
+                print("a")
+        # End epoch
+        accelerator.wait_for_everyone()
+        if accelerator.is_main_process:
+            print("This is main_process")
+            print(epoch)
+            print((epoch + 1) % config["save_per_epochs"])
+            if (epoch + 1) % config["save_per_epochs"] == 0:
+                print("Save model epoch")
+
+def main(args):
+    writer = SummaryWriter(log_dir=args.log_dir)
+    config = json.load(open(args.config_file, "r"))
+    
+    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    accelerator = Accelerator(
+        gradient_accumulation_steps=config["gradient_accumulation_steps"],
+        kwargs_handlers=[ddp_kwargs]
+    )
+    
+    # scheduler
+    noise_scheduler = DDPMScheduler(
+        num_train_timesteps=1000,
+        beta_schedule="linear",
+        prediction_type="epsilon"
+    )
+    
+    model = TTSMultiSpeaker2D(config)
+        
+    # optimizer
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=1e-5,
+        betas=(0.95, 0.999),
+        weight_decay=1e-6,
+        eps=1e-8
+    )
+    
+    # data
+    dataloader = create_dataloader(
+        args.data_files,
+        args.batch_size,
+        args.max_seq_length,
+        data_type="multi_speaker",
+        use_tar=args.use_tar,
+        shuffle=True
+    )
+    
+    num_update_steps_per_epoch = math.ceil(len(dataloader) / config["gradient_accumulation_steps"])
+    max_train_steps = config["num_train_epochs"] * num_update_steps_per_epoch
+        
+    lr_scheduler = get_scheduler(
+        config["lr_scheduler"],
+        optimizer=optimizer,
+        num_warmup_steps=config["lr_warmup_steps"] * config["gradient_accumulation_steps"],
+        num_training_steps=max_train_steps * config["gradient_accumulation_steps"],
+    )
+    
+    dataloader, model, optimizer, lr_scheduler = accelerator.prepare(
+        dataloader, model, optimizer, lr_scheduler
+    )
+    
+    if args.model_ckpt is not None and args.optim_ckpt is not None:
+        model.load_state_dict(
+            torch.load(args.model_ckpt, map_location=accelerator.device)
+        )
+        optimizer.load_state_dict(
+            torch.load(args.optim_ckpt, map_location=accelerator.device)
+        )
+        accelerator.load_state(args.ckpt_dir)
+    
+    # training loop
+    global_step = 0
+    for epoch in range(config["num_train_epochs"]):
+        epoch += args.prev_epoch
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader, disable=not accelerator.is_local_main_process)
+        model.train()
+        
+        for batch in pbar:
+            with accelerator.accumulate(model):
+                codes = batch["code"]
+                text_seq_ids = batch["cmu_sequence"]
+                spk_code = batch["sample"]
+                code_length = batch["code_length"]
+                
+                # Sample noise
+                for i in range(len(codes)):
+                    codes[i] = rearrange(codes[i].squeeze(), "l n -> n l")
+                codes = rearrange(
+                    pad_sequence(codes, batch_first=True),
+                    "b n l -> b l n"
+                ).unsqueeze(1)
+                                  
+                noise = torch.randn_like(codes).to(accelerator.device)
+                
+                bsz = codes.shape[0]
+                
+                timesteps = torch.randint(
+                    0, noise_scheduler.config.num_train_timesteps, (bsz,),
+                    device=accelerator.device
+                ).long()
+                
+                # Add noise
+                noisy_codes = noise_scheduler.add_noise(
+                    codes, noise, timesteps
+                )
+                
+                output_unet, output_dp = model(
+                    noisy_codes,
+                    timesteps,
+                    text_seq_ids,
+                    spk_code
+                )
+                
+                unet_loss = F.mse_loss(
+                    output_unet.sample.float(),
+                    noise.float(), reduction="mean"
+                )
+                dp_loss = F.mse_loss(
+                    output_dp.sample.float(),
+                    code_length.float(), reduction="mean"
+                )
+                
+                loss = sum([unet_loss, dp_loss])
+                
+                # Gather the losses across all processes for logging (if we use distributed training).
+                avg_loss = accelerator.gather(loss.repeat(args.batch_size)).mean()
+                train_loss = avg_loss.item() / config["gradient_accumulation_steps"]
+                
+                writer.add_scalar("Loss/train", train_loss, global_step)
+                
+                accelerator.backward(loss)
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            # End step
+            pbar.set_postfix(MSE=train_loss)
+            global_step += 1
+        
+        # End epoch
+        accelerator.wait_for_everyone()
+        if accelerator.is_main_process:
+            unwrap_model = accelerator.unwrap_model(model)
+            accelerator.save_state(output_dir=args.ckpt_dir)
+            if (epoch + 1) % config["save_per_epochs"] == 0:
+                accelerator.save(unwrap_model.state_dict(), args.ckpt_dir + f"ckpt_{epoch+1}.pt")
+                accelerator.save(optimizer.state_dict(), args.ckpt_dir + f"optim_{epoch+1}.pt")
+                accelerator.save_state(output_dir=args.ckpt_dir)
+            
+    writer.flush()
+    writer.close()
+    
+    
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Train TTS models."
+                                     "The data is stored in WebDataset format.")
+    parser.add_argument('--data_files', type=str, default=None,
+                        help="Path to the training data files. Separate by ','.\
+                            data_file1,data_file2",
+                        required=True)
+    parser.add_argument('--log_dir', type=str,
+                        help="Directory to save logs.", required=True)
+    parser.add_argument('--config_file', type=str,
+                        help="Path to config file.", required=True)
+    parser.add_argument('--ckpt_dir', type=str,
+                        help="Directory to save checkpoints.", required=True)
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help="Batch size of Encodec encode.")
+    parser.add_argument('--max_seq_length', type=int, default=550,
+                        help="Maximum length of cmu sequence.")
+    parser.add_argument('--model_ckpt', type=str, default=None,
+                        help="Path to the previous model checkpoint.")
+    parser.add_argument('--optim_ckpt', type=str, default=None,
+                        help="Path to the previous optimizer checkpoint.")
+    parser.add_argument('--prev_epoch', type=int, default=0,
+                        help="Num trained epoch.")
+    parser.add_argument('--use_tar', action='store_true')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
+    
--- tts/model/models.py
+++ tts/model/models.py
@@ -1,10 +1,11 @@
 from einops import rearrange
-from functools import partial
 import numpy as np
 from typing import Union, Dict, Any, Optional, List
 
 import torch
 from torch import nn
+
+from diffusers.models.unet_2d_condition import UNet2DConditionModel
 
 from tts.ldm.unet_1d_condition import Unet1DConditionModel
 from tts.model.modules import (
@@ -58,7 +59,7 @@ class TTSSingleSpeaker(nn.Module):
             sample=sample,
             timestep=timestep,
             encoder_hidden_states=text_emb,
-            attention_mask=attention_mask,
+            # attention_mask=attention_mask,
             cross_attention_kwargs=cross_attention_kwargs,
             return_dict=return_dict
         )
@@ -66,7 +67,7 @@ class TTSSingleSpeaker(nn.Module):
         return unet_output
     
     
-class TTSMultiSpeaker(nn.Module):
+class TTSMultiSpeaker1D(nn.Module):
     def __init__(
         self,
         config,
@@ -107,7 +108,7 @@ class TTSMultiSpeaker(nn.Module):
         sample: torch.FloatTensor,
         timestep: Union[torch.Tensor, float, int],
         text_seq_ids: List[torch.Tensor],
-        spk_code: List[torch.Tensor],
+        spk_code: List[np.ndarray],
         cross_attention_kwargs: Optional[Dict[str, Any]]=None,
         return_dict: bool=True,
     ):
@@ -125,7 +126,7 @@ class TTSMultiSpeaker(nn.Module):
         dp_output = self.duration_predictor(x_dp, m_dp)
         
         for i in range(len(spk_code)):
-            spk_code[i] = rearrange(spk_code[i], "l n -> n l")
+            spk_code[i] = rearrange(spk_code[i].squeeze(), "l n -> n l")
         spk_emb_list = self.spk_encoder(spk_code)
 
         input_list = _samplewise_merge_tensors(text_emb_list, spk_emb_list)
@@ -187,3 +188,113 @@ class TTSMultiSpeaker(nn.Module):
         
         return unet_output
     
+
+class TTSMultiSpeaker2D(nn.Module):
+    def __init__(
+        self,
+        config,
+    ) -> None:
+        super().__init__()
+        self.text_encoder = TextEmbedding(
+            config["cmu_vocab_len"],
+            config["cross_attention_dim"]
+        )
+        
+        self.duration_predictor = DurationPredictor(
+            in_channels=config["cross_attention_dim"],
+            out_channels=config["out_channels_dp"],
+            kernel_size=config["kernel_size"],
+            p_dropout=config["p_dropout"]
+        )
+
+        self.unet = UNet2DConditionModel(
+            in_channels=config["in_channels"],
+            out_channels=config["out_channels"],
+            layers_per_block=config["layers_per_block"],
+            block_out_channels=config["block_out_channels"],
+            down_block_types=config["down_block_types"],
+            up_block_types=config["up_block_types"],
+            cross_attention_dim=config["cross_attention_dim"],
+        )
+        
+        self.spk_encoder = SpeakerEncoder(
+            8,
+            1024,
+            config["cross_attention_dim"]
+        )
+        
+    def forward(
+        self,
+        sample: torch.FloatTensor,
+        timestep: Union[torch.Tensor, float, int],
+        text_seq_ids: List[torch.Tensor],
+        spk_code: List[np.ndarray],
+    ):
+        text_emb_list = self.text_encoder(text_seq_ids) # list of [l, d]
+        
+        x_dp, m_dp = list_to_tensor(text_emb_list) # [b, l, d], [b, l, 1]
+        x_dp = torch.transpose(x_dp, 1, -1)
+        x_dp = torch.detach(x_dp)
+        m_dp = torch.transpose(m_dp, 1, -1)
+        
+        #TODO:
+        # 1. Finish the flow with duration predictor (in review)
+        # 2. Define a function to predict duration (in review)
+        # 3. Define a cuntion to get encoder hidden states (in review)
+        dp_output = self.duration_predictor(x_dp, m_dp)
+        
+        for i in range(len(spk_code)):
+            spk_code[i] = rearrange(spk_code[i].squeeze(), "l n -> n l")
+        spk_emb_list = self.spk_encoder(spk_code)
+
+        input_list = _samplewise_merge_tensors(text_emb_list, spk_emb_list)
+        
+        x, _ = list_to_tensor(input_list)
+        
+        unet_output = self.unet(
+            sample=sample,
+            timestep=timestep,
+            encoder_hidden_states=x,
+        )
+        
+        return unet_output, dp_output
+    
+    def predict_duration(
+        self,
+        text_seq_ids: List[torch.Tensor]
+    ):
+        text_emb_list = self.text_encoder(text_seq_ids) # list of [l, d]
+        
+        x_dp, m_dp = list_to_tensor(text_emb_list) # [b, l, d], [b, l, 1]
+        x_dp = torch.transpose(x_dp, 1, -1)
+        x_dp = torch.detach(x_dp)
+        m_dp = torch.transpose(m_dp, 1, -1)
+        
+        dp_output = self.duration_predictor(x_dp, m_dp)
+        
+        return dp_output
+    
+    def predict_unet(
+        self,
+        sample: torch.FloatTensor,
+        timestep: Union[torch.Tensor, float, int],
+        text_seq_ids: List[torch.Tensor],
+        spk_code: List[torch.Tensor],
+    ):
+        text_emb_list = self.text_encoder(text_seq_ids) # list of [l, d]
+        
+        for i in range(len(spk_code)):
+            spk_code[i] = rearrange(spk_code[i], "l n -> n l")
+        spk_emb_list = self.spk_encoder(spk_code)
+
+        input_list = _samplewise_merge_tensors(text_emb_list, spk_emb_list)
+        
+        x, _ = list_to_tensor(input_list)
+        
+        unet_output = self.unet(
+            sample=sample,
+            timestep=timestep,
+            encoder_hidden_states=x,
+        )
+        
+        return unet_output
